29ab7f234546680fb2db3f078cd500020eed108c
shyam <shyammohankanojia@gmail.com>
2014-04-07 18:14:06 +0530

    first commit

---
+++ src/chapter3/main.go
@@ -0,0 +1,7 @@
+package main 
+import "fmt"
+func main() {
+	fmt.Println("Sum of 1 + 1 = ", 1.0 + 1.1)
+	
+}
+
---
+++ src/hello/playground.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+				"fmt"
+				// "net"
+				// "os"
+				"time"
+				"newmath"
+			)
+
+func main(){
+	fmt.Println("Welcome to the Playground!")
+	fmt.Println("The time is", time.Now())
+
+	fmt.Println("Hello world : Sqrt(2) = ", newmath.Sqrt(2))
+}
